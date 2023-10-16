a58e4832909dba6944be55c10eaf9ce1f25ad54f
Daniel Hörnqvist <daniel.hornqvist@gmail.com>
2023-10-16 12:02:51 +0200

    fixa desktop version

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ function scrollFunction() {
     document.getElementById("meny").style.fontSize = "4rem";
     document.getElementById("logo").style.width = "110px";
     document.getElementById("head_container").style.padding = "11rem";
-    document.getElementById("aside-menu").style.paddingTop = "11rem";
-    document.getElementById("aside-menu").style.width = "16rem";
+    document.querySelector(".aside-menu-container").style.paddingTop = "11rem";
+    document.querySelector(".aside-menu-container").style.width = "16rem";
     document.getElementById("price-filter").style.fontSize = "15px";
     document.getElementById("reset-filter").style.fontSize = "25px";
     document.getElementById("reset-filter").style.width = "190px";
@@ -53,8 +53,8 @@ function scrollFunction() {
     document.getElementById("meny").style.fontSize = "8rem";
     document.getElementById("logo").style.width = "225px";
     document.getElementById("head_container").style.padding = "18rem";
-    document.getElementById("aside-menu").style.paddingTop = "18rem";
-    document.getElementById("aside-menu").style.width = "20rem";
+    document.querySelector(".aside-menu-container").style.paddingTop = "18rem";
+    document.querySelector(".aside-menu-container").style.width = "20rem";
     document.getElementById("price-filter").style.fontSize = "17px";
     document.getElementById("reset-filter").style.fontSize = "27px";
     document.getElementById("reset-filter").style.width = "300px";
@@ -64,14 +64,17 @@ function scrollFunction() {
 }
 
 //funktion för att sortera matdatan efter pris --Alma
-function sortDishesByPrice(low) { 
-  foodData.sort((a,b) => { //sort metod på foodData array som sorterar elementen med en jämförelsefunktion
-    const priceA = parseFloat(a.price.replace(" kr", "")); 
+function sortDishesByPrice(low) {
+  foodData.sort((a, b) => {
+    //sort metod på foodData array som sorterar elementen med en jämförelsefunktion
+    const priceA = parseFloat(a.price.replace(" kr", ""));
     const priceB = parseFloat(b.price.replace(" kr", ""));
-    
-    if(low) { //om low är sant, sorteras priset från lägre till högre
+
+    if (low) {
+      //om low är sant, sorteras priset från lägre till högre
       return priceA - priceB;
-    }else { // om low är falskt, sorteras priser från högre till lägre
+    } else {
+      // om low är falskt, sorteras priser från högre till lägre
       return priceB - priceA;
     }
   });
@@ -106,40 +109,43 @@ async function createBoxes() {
     description.textContent = data.description.swe;
     box.appendChild(description);
 
-    gridContainer.appendChild(box); //elementen läggs till i gridContainer  
+    gridContainer.appendChild(box); //elementen läggs till i gridContainer
   });
 }
 
- function updateBoxes(data) { //funktion för att uppdatera boxarna av maträttsdata
-  while (gridContainer.firstChild) { 
+function updateBoxes(data) {
+  //funktion för att uppdatera boxarna av maträttsdata
+  while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild); //rensa bort tidigare maträttsboxar
   }
 
-  data.forEach((item) => { 
-    const box = createDishBox(item); 
-    gridContainer.appendChild(box); 
+  data.forEach((item) => {
+    const box = createDishBox(item);
+    gridContainer.appendChild(box);
   });
-} 
+}
 // Anropa funktionen för att skapa boxar när sidan laddas
 createBoxes();
 
 const priceFilterSelect = document.getElementById("price-filter");
 
-priceFilterSelect.addEventListener("change", () => { //lyssnar på ändring som användaren gör på sortera knapp
+priceFilterSelect.addEventListener("change", () => {
+  //lyssnar på ändring som användaren gör på sortera knapp
   const selectOption = priceFilterSelect.value;
 
-  if(selectOption === "low") { //beroende på val av användaren, anropas sortDishesByPrice funktionen med antingen sant eller falskt.
+  if (selectOption === "low") {
+    //beroende på val av användaren, anropas sortDishesByPrice funktionen med antingen sant eller falskt.
     sortDishesByPrice(true);
-  }else if(selectOption === "high") {
+  } else if (selectOption === "high") {
     sortDishesByPrice(false);
   }
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild);
   }
 
-  updateBoxes(foodData); //anropar funktion för att uppdatera maträttsboxarna 
-  });
- 
+  updateBoxes(foodData); //anropar funktion för att uppdatera maträttsboxarna
+});
+
 // Funktion för att skapa maträttsbox
 function createDishBox(data) {
   const box = document.createElement("div");
@@ -165,91 +171,99 @@ function createDishBox(data) {
   box.appendChild(description);
 
   return box;
-} 
+}
 
 //Byta språk
-document.getElementById("byta").addEventListener("click",() =>{
+document.getElementById("byta").addEventListener("click", () => {
   document.getElementById("byta").innerHTML = foodData[0].dish.en;
 
-  if(document.querySelector("aside").classList.contains("swe")){
+  if (document.querySelector("aside").classList.contains("swe")) {
     /**Kollar efter id och byter sedan texten från langData beroende på om aside elementet har klassen swe eller eng */
     document.getElementById("reset-filter").textContent = langData["eng"].title;
-    document.getElementById("low-to-high").textContent = langData["eng"].low_to_high;
-    document.getElementById("high-to-low").textContent = langData["eng"].high_to_low;
+    document.getElementById("low-to-high").textContent =
+      langData["eng"].low_to_high;
+    document.getElementById("high-to-low").textContent =
+      langData["eng"].high_to_low;
     document.getElementById("price").textContent = langData["eng"].price;
 
     document.getElementById("category").textContent = langData["eng"].category;
     document.getElementById("label_vego").innerHTML = langData["eng"].vego;
-    document.getElementById("label_chicken").textContent = langData["eng"].chicken;
+    document.getElementById("label_chicken").textContent =
+      langData["eng"].chicken;
 
     document.getElementById("label_pork").textContent = langData["eng"].pork;
     document.getElementById("label_beef").textContent = langData["eng"].meat;
     document.getElementById("label_fish").textContent = langData["eng"].fish;
 
     document.getElementById("allergy_id").innerHTML = langData["eng"].allergy;
-    document.getElementById("label_gluten").textContent = langData["eng"].gluten;
-    document.getElementById("label_lactose").textContent = langData["eng"].lactose;
+    document.getElementById("label_gluten").textContent =
+      langData["eng"].gluten;
+    document.getElementById("label_lactose").textContent =
+      langData["eng"].lactose;
 
     document.getElementById("byta").textContent = "Change to Swedish";
     document.querySelector("aside").classList.remove("swe");
     document.querySelector("aside").classList.add("eng");
-
-  }else if(document.querySelector("aside").classList.contains("eng")){
-
+  } else if (document.querySelector("aside").classList.contains("eng")) {
     document.getElementById("reset-filter").textContent = langData["swe"].title;
-    document.getElementById("low-to-high").textContent = langData["swe"].low_to_high;
-    document.getElementById("high-to-low").textContent = langData["swe"].high_to_low;
+    document.getElementById("low-to-high").textContent =
+      langData["swe"].low_to_high;
+    document.getElementById("high-to-low").textContent =
+      langData["swe"].high_to_low;
     document.getElementById("price").textContent = langData["swe"].price;
 
     document.getElementById("category").textContent = langData["swe"].category;
     document.getElementById("label_vego").textContent = langData["swe"].vego;
-    document.getElementById("label_chicken").textContent = langData["swe"].chicken;
+    document.getElementById("label_chicken").textContent =
+      langData["swe"].chicken;
 
     document.getElementById("label_pork").textContent = langData["swe"].pork;
     document.getElementById("label_beef").textContent = langData["swe"].meat;
     document.getElementById("label_fish").textContent = langData["swe"].fish;
 
     document.getElementById("allergy_id").textContent = langData["swe"].allergy;
-    document.getElementById("label_gluten").textContent = langData["swe"].gluten;
-    document.getElementById("label_lactose").textContent = langData["swe"].lactose;
+    document.getElementById("label_gluten").textContent =
+      langData["swe"].gluten;
+    document.getElementById("label_lactose").textContent =
+      langData["swe"].lactose;
 
     document.getElementById("byta").textContent = "Byt till Engelska";
     document.querySelector("aside").classList.remove("eng");
     document.querySelector("aside").classList.add("swe");
   }
-})
-const langData ={
-  "eng":{
-    "title": "Reset filter",
-    "low_to_high": "Low To High",
-    "high_to_low": "High to Low",
-    "price": "Price",
-    "category":"Category",
-    "vego": "Vegetarien",
-    "chicken" : "Chicken",
-    "pork": "Pork",
-    "meat": "Beef",
-    "fish": "Fish", 
-    "allergy": "Allergy",
-    "gluten": "Glutenfree",
-    "lactose": "Lactosfree"
+});
+const langData = {
+  eng: {
+    title: "Reset filter",
+    low_to_high: "Low To High",
+    high_to_low: "High to Low",
+    price: "Price",
+    category: "Category",
+    vego: "Vegetarien",
+    chicken: "Chicken",
+    pork: "Pork",
+    meat: "Beef",
+    fish: "Fish",
+    allergy: "Allergy",
+    gluten: "Glutenfree",
+    lactose: "Lactosfree",
   },
-  "swe":{
-    "title": "Återställ filter",
-    "low_to_high": "Lågt till Högt",
-    "high_to_low": "Högt till lågt",
-    "price": "Pris",
-    "category": "Kategorier",
-    "vego": "Vegetariskt",
-    "chicken" : "Kyckling",
-    "pork": "Fläsk",
-    "meat": "Biff",
-    "fish": "Fisk",
-    "allergy": "Allergier",
-    "gluten": "Glutenfri",
-    "lactose": "Laktosfri"
-  }
-}
+  swe: {
+    title: "Återställ filter",
+    low_to_high: "Lågt till Högt",
+    high_to_low: "Högt till lågt",
+    price: "Pris",
+    category: "Kategorier",
+    vego: "Vegetariskt",
+    chicken: "Kyckling",
+    pork: "Fläsk",
+    meat: "Biff",
+    fish: "Fisk",
+    allergy: "Allergier",
+    gluten: "Glutenfri",
+    lactose: "Laktosfri",
+  },
+};
 
 const slider = document.querySelector(".carousel-wrapper");
 let isDown = false;
